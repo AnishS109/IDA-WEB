7b8cde19163ac237036aa52c66321f6849182de1
Anish_Saini <anishsaini9098@gmail.com>
2025-01-05 17:53:15 +0530

    Update SalesHome.jsx

--- Client/src/Sales_ALL/SalesHome.jsx
+++ Client/src/Sales_ALL/SalesHome.jsx
@@ -15,7 +15,7 @@ import EnrolledStudent from "./components/EnrolledStudent";
 
 const SalesHome = () => {
 
-  const [selectedOption, setSelectedOption] = useState(localStorage.getItem("selectedOption") || "Add Enquiry");
+  const [selectedOption, setSelectedOption] = useState("Add Enquiry");
 
   const options = [
     { label: "Add Enquiry", icon: <AddIcon />, key: "Add Enquiry" },
@@ -24,10 +24,6 @@ const SalesHome = () => {
     { label: "Confirmed", icon: <CheckCircleOutlineIcon />, key: "Confirmed" },
     { label: "Enrolled Student", icon: <SchoolIcon />, key: "Enrolled Student" }
   ];
-
-  useEffect(() => {
-    localStorage.setItem("selectedOption", selectedOption);
-  }, [selectedOption]);
 
   const Content = () => {
     if (selectedOption === "Add Enquiry") {
