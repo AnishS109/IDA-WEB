f1f2d8c60d8d61d7224743c23cf112b101e53ee7
AnishS109 <anishsaini9098@gmail.com>
2025-01-08 23:03:52 +0530

    minorChanges

--- Server/controllers/addEnquiry.js
+++ Server/controllers/addEnquiry.js
@@ -3,15 +3,17 @@ import addEnquirySchema from "../models/addEnquirySchema.js"
 // -------------- Add Enquiry Details (POST REQUEST)---------------
 
 export const addEnquiry = async(req, res) => {
-  const { ...otherDetails } = req.body
-  // console.log(req.body);
-  
+  const {salesName, ...otherDetails} = req.body
+
   try {
+    if(!salesName){
+      return res.status(404).json({message:"Something went wrong, PLease Login again!"})
+    }
 
-    const student = new addEnquirySchema({ ...otherDetails })
+    const student = new addEnquirySchema({salesName, ...otherDetails})
     await student.save()
-    res.status(200).json({message:"SuccessFully Submitted"})
 
+    res.status(200).json({message:"SuccessFully Submitted"})
   } catch (error) {
     console.error("Error while saving addEnquiry Details", error)
     return res.status(500).json({message:"Error while saving addEnquiry Details"})
